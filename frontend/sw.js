b5ca2b9b14e2c748c7edb85bc99de097f7b0283e
Robin Thellend <rthellend@gmail.com>
2026-01-27 13:14:42 -0800

    v0.1.134

--- frontend/sw.js
+++ frontend/sw.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const CACHE_NAME = 'skorekeeper-v0.1.130';
+const CACHE_NAME = 'skorekeeper-v0.1.134';
 const CORE_ASSETS = [
     './',
     './.sso/proxy.mjs',
