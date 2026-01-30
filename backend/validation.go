6635ae5c76dfdae7ea8c85a89c2855e0b4e49aaf
Robin Thellend <rthellend@gmail.com>
2026-01-30 11:31:26 -0800

    v0.1.136

--- backend/validation.go
+++ backend/validation.go
@@ -39,7 +39,7 @@ func isValidEmail(email string) bool {
 const (
 	CurrentSchemaVersion   = 3
 	CurrentProtocolVersion = 1
-	CurrentAppVersion      = "0.1.135"
+	CurrentAppVersion      = "0.1.136"
 )
 
 // ActionTypes constants
